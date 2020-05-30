2d457d71d43f0686a3fa7c38dff75d1965a0d28b
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2020-05-30 23:24:15 +0800

    LR by PyTorch

--- Issue10/LinearRegression.py
+++ Issue10/LinearRegression.py
@@ -1,14 +1,18 @@
 # -*- coding:utf-8  -*-
 import torch
+from torch import nn
 from IPython import display
 from matplotlib import pyplot as plt
 import numpy as np
 import random
 import logging
+import torch.utils.data as Data
+from torch.nn import init
+import torch.optim as optim
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s : %(message)s')
-
+torch.manual_seed(1)  # 固定 pytorch 的随机数
 NUM_IMPUTS = 2  # 数据的维度
 BATCH_SIZE = 10
 
@@ -48,8 +52,19 @@ class Dataset():
             j = torch.LongTensor(indices[i: min(i + batch_size, num)])
             yield self.features.index_select(0, j), self.labels.index_select(0, j)
 
+    def data_loader_torch(self, batch_size=10):
+        ''' 利用 torch 加载数据集 '''
+        dataset = Data.TensorDataset(self.features, self.labels)
+        data_iter = Data.DataLoader(
+            dataset=dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=2,
+        )
+        return data_iter
 
-class LinerRegOriginal():
+
+class LinearRegressionOriginal():
     ''' 从零开始做线性回归 '''
     def __init__(self, lr=0.03, epochs=5, batch_size=10):
         self.lr = lr
@@ -76,7 +91,7 @@ class LinerRegOriginal():
         :param y: 真实值
         :return: loss 值
         '''
-        return ((y_hat - y.view(y_hat.size())) ** 2).sum() / 2
+        return ((y_hat - y.view(y_hat.size())) ** 2 / 2).mean()
 
     def sgd(self):
         ''' 定义优化函数 '''
@@ -87,7 +102,7 @@ class LinerRegOriginal():
         features, labels = dataset.features, dataset.labels
         # 开始训练
         for epoch in range(self.epochs):
-            for X, y in dataset.data_loader():
+            for (X, y) in dataset.data_loader():
                 loss = self.squared_loss(self.linreg(X), y)
                 loss.backward()
                 self.sgd()
@@ -98,6 +113,45 @@ class LinerRegOriginal():
             logging.info('epoch %d, loss %f' % (epoch + 1, train_loss.item()))
 
 
+class LinearNet(nn.Module):
+    ''' 定义线性回归模型 '''
+    def __init__(self, n_feature=2):
+        super(LinearNet, self).__init__()
+        self.linear = nn.Linear(n_feature, 1)
+
+    def forward(self, x):
+        y = self.linear(x)
+        return y
+
+
+class LinearRegressionPytorch():
+    ''' 使用 pytorch 实现的线性分类器 '''
+    def __init__(self, num_inputs=2, lr=0.03, epochs=5):
+        self.epochs = epochs
+        self.net = LinearNet(num_inputs)
+        self.init_coef()
+        self.loss = nn.MSELoss()  # 定义损失函数
+        self.optimizer = optim.SGD(self.net.parameters(), lr=lr)  # 定义优化函数
+
+    def init_coef(self):
+        ''' 初始化模型参数 '''
+        init.normal_(self.net.linear.weight, mean=0.0, std=0.01)
+        init.constant_(self.net.linear.bias, val=0.0)
+
+    def train(self, dataset):
+        for epoch in range(1, self.epochs + 1):
+            for X, y in dataset.data_loader_torch():
+                output = self.net(X)
+                loss = self.loss(output, y.view(-1, 1))
+                self.optimizer.zero_grad()  # 重置梯度
+                loss.backward()
+                self.optimizer.step()
+            logging.info('epoch %d, loss: %f' % (epoch, loss.item()))
+
+
 if __name__ == '__main__':
     dataset = Dataset()
-    LinerRegOriginal().train(dataset)
+    logging.info(f'从零开始的线性回归')
+    LinearRegressionOriginal().train(dataset)
+    logging.info(f'使用 PyTorch 的线性回归')
+    LinearRegressionPytorch().train(dataset)
