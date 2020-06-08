235739249cffa3ec81cd3474dd92e2d8be21eaad
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2020-06-08 23:14:28 +0800

    Multivariate Function Fitting

--- Issue10/task02/MFF.py
+++ Issue10/task02/MFF.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import torch
+import torch.utils.data as Data
 import numpy as np
 import matplotlib.pyplot as plt
 import logging
@@ -29,13 +30,46 @@ def show_data(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
 
 
 class MultivariateFunctionFitting():
-    def __init__(self):
-        super().__init__()
+    def __init__(self, train_data, test_data, epochs=100, loss=None):
+        # 初始化数据
+        self.train_features = train_data[0]
+        self.train_labels = train_data[1]
+        self.test_features = test_data[0]
+        self.test_labels = test_data[1]
+        self.epochs = epochs
+        self.loss = loss if loss else torch.nn.MSELoss()
+        # 初始化网络模型
+        self.net = torch.nn.Linear(self.train_features.shape[-1], 1)
+        # 设置批量大小
+        self.batch_size = min(10, self.train_labels.shape[0])
+        self.dataset = Data.TensorDataset(self.train_features, self.train_labels)
+        self.train_iter = Data.DataLoader(self.dataset, self.batch_size, shuffle=True)
+        self.optimizer = torch.optim.SGD(self.net.parameters(), lr=0.01)
+
+    def train(self):
+        train_loss, test_loss = [], []
+        for epoch in range(self.epochs):
+            for X, y in self.train_iter:
+                loss = self.loss(self.net(X), y.view(-1, 1))
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+            train_labels = self.train_labels.view(-1, 1)
+            test_labels = self.test_labels.view(-1, 1)
+            train_epoch_ls = self.loss(self.net(self.train_features), train_labels).item()
+            test_epoch_ls = self.loss(self.net(self.test_features), test_labels).item()
+            logging.info(f'epoch {epoch} \t train loss: {train_epoch_ls} \t' +
+                         f'test loss: {test_epoch_ls}')
+            train_loss.append(train_epoch_ls)
+            test_loss.append(test_epoch_ls)
+        logging.info(f'final epoch: train loss: {train_loss[-1]}\t test loss: {test_loss[-1]}')
+        show_data(range(1, self.epochs+1), train_loss, 'epochs', 'loss',
+                  range(1, self.epochs+1), test_loss, ['train', 'test'])
+        logging.info(f'weight: {self.net.weight.data} \n bias: {self.net.bias.data}')
 
 
 if __name__ == '__main__':
     features, labels = data_generator()
-    logging.info(f'features: {features}')
-    logging.info(f'labels: {labels}')
-    show_data(features.numpy(), labels.numpy(), 'features', 'labels')
-    MultivariateFunctionFitting()
+    train_data = (features[:100], labels[:100])
+    test_data = (features[100:], labels[100:])
+    MultivariateFunctionFitting(train_data, test_data).train()
