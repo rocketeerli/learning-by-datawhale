4a678931d0e62824ef108e24cf9e6e17b6adf5be
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2020-06-18 00:33:22 +0800

    finish CNN Advanced

--- Issue10/task03/cnn.py
+++ Issue10/task03/cnn.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s: %(message)s')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 NUM_CLASSES = 10
-lr, epochs, batch_size = 0.001, 5, 256
+lr, epochs, batch_size = 0.001, 5, 128
 
 # VGG 网络参数
 ratio = 8
@@ -149,6 +149,53 @@ class NiN():
         return blk
 
 
+class Inception(nn.Module):
+    def __init__(self, in_c, c1, c2, c3, c4):
+        super().__init__()
+        # 线路1，单1 x 1卷积层
+        self.p1_1 = nn.Conv2d(in_c, c1, kernel_size=1)
+        # 线路2，1 x 1卷积层后接3 x 3卷积层
+        self.p2_1 = nn.Conv2d(in_c, c2[0], kernel_size=1)
+        self.p2_2 = nn.Conv2d(c2[0], c2[1], kernel_size=3, padding=1)
+        # 线路3，1 x 1卷积层后接5 x 5卷积层
+        self.p3_1 = nn.Conv2d(in_c, c3[0], kernel_size=1)
+        self.p3_2 = nn.Conv2d(c3[0], c3[1], kernel_size=5, padding=2)
+        # 线路4，3 x 3最大池化层后接1 x 1卷积层
+        self.p4_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.p4_2 = nn.Conv2d(in_c, c4, kernel_size=1)
+
+    def forward(self, x):
+        p1 = F.relu(self.p1_1(x))
+        p2 = F.relu(self.p2_2(F.relu(self.p2_1(x))))
+        p3 = F.relu(self.p3_2(F.relu(self.p3_1(x))))
+        p4 = F.relu(self.p4_2(self.p4_1(x)))
+        return torch.cat((p1, p2, p3, p4), dim=1)  # 在通道维上连结输出
+
+
+class GoogLeNet():
+    def __init__(self):
+        self.b1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+                                nn.ReLU(),
+                                nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        self.b2 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1),
+                                nn.Conv2d(64, 192, kernel_size=3, padding=1),
+                                nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        self.b3 = nn.Sequential(Inception(192, 64, (96, 128), (16, 32), 32),
+                                Inception(256, 128, (128, 192), (32, 96), 64),
+                                nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        self.b4 = nn.Sequential(Inception(480, 192, (96, 208), (16, 48), 64),
+                                Inception(512, 160, (112, 224), (24, 64), 64),
+                                Inception(512, 128, (128, 256), (24, 64), 64),
+                                Inception(512, 112, (144, 288), (32, 64), 64),
+                                Inception(528, 256, (160, 320), (32, 128), 128),
+                                nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        self.b5 = nn.Sequential(Inception(832, 256, (160, 320), (32, 128), 128),
+                                Inception(832, 384, (192, 384), (48, 128), 128),
+                                GlobalAvgPool2d())
+        self.net = nn.Sequential(self.b1, self.b2, self.b3, self.b4, self.b5,
+                                 Flatten(), nn.Linear(1024, NUM_CLASSES))
+
+
 def test(net: nn.Module):
     ''' 测试模型 '''
     X = torch.rand(1, 1, 224, 224).to(device)
@@ -176,3 +223,8 @@ if __name__ == '__main__':
     nin = NiN().net.to(device)
     optimizer = optim.Adam(nin.parameters(), lr=2*lr)
     train(nin, dataset, criterion, optimizer)
+
+    logging.info('GoogLeNet ...')
+    googLeNet = GoogLeNet().net.to(device)
+    optimizer = optim.Adam(googLeNet.parameters(), lr=2*lr)
+    train(googLeNet, dataset, criterion, optimizer)
