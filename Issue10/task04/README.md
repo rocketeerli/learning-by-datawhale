877dcf8e03dd25c5b4bb0402d53a4b8a295271f1
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2020-06-20 17:29:41 +0800

    text preprocess

--- Issue10/task04/README.md
+++ Issue10/task04/README.md
@@ -13,4 +13,18 @@
 3. 建立字典，将每个词映射到一个唯一的索引（index）
 4. 将文本从词的序列转换为索引的序列，方便输入模型
 
+* 分词的工具库
 
+**spaCy**：
+
+	import spacy
+	nlp = spacy.load('en_core_web_sm')
+	doc = nlp(text)
+	print([token.text for token in doc])
+
+**NLTK**：
+
+	from nltk.tokenize import word_tokenize
+	from nltk import data
+	data.path.append('/home/kesci/input/nltk_data3784/nltk_data')
+	print(word_tokenize(text))
--- Issue10/task04/text_preprocessing.py
+++ Issue10/task04/text_preprocessing.py
@@ -3,6 +3,9 @@ import collections
 import re
 import os
 import logging
+import spacy
+from nltk.tokenize import word_tokenize
+from nltk import data
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s: %(message)s')
@@ -27,7 +30,32 @@ class Vocab():
     ''' 构建一个字典（vocabulary），将每个词映射到一个唯一的索引编号 '''
     def __init__(self, tokens, min_freq=0, use_special_tokens=False):
         counter = self.count_corpus(tokens)
-        self.token_freqs = list(counter.item())
+        self.token_freqs = counter.items()
+        self.index_to_token = []
+        if use_special_tokens:
+            self.pad, self.bos, self.eos, self.unk = (0, 1, 2, 3)
+            self.index_to_token += ['', '', '', '']
+        else:
+            self.unk = 0
+            self.index_to_token += ['']
+        self.index_to_token += [token for token, freq in self.token_freqs
+                            if freq >= min_freq and token not in self.index_to_token]
+        self.token_to_indx = dict()
+        for index, token in enumerate(self.index_to_token):
+            self.token_to_indx[token] = index
+
+    def __len__(self):
+        return len(self.index_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_indx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.index_to_token[indices]
+        return [self.index_to_token[index] for index in indices]
 
     @staticmethod
     def count_corpus(sentences):
@@ -43,3 +71,19 @@ if __name__ == '__main__':
     tokens = tokenize(lines)
     logging.info(f'tokens: {tokens[:5]}')
     # 3. 建立字典
+    vocab = Vocab(tokens)
+    logging.info(list(vocab.token_to_indx.items())[0:10])
+    for i in range(8, 10):
+        logging.info(f'words: {tokens[i]}')
+        logging.info(f'indices: {vocab[tokens[i]]}')
+    # 高级的分词方法
+    # spaCy
+    text = "Mr. Chen doesn't agree with my suggestion. Mr. Chen does agree with my suggestion."
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(text)
+    logging.info(f'type of doc {type(doc)}')
+    logging.info(f'spacy: {[token.text for token in doc]}')
+    logging.info(list(doc.sents))
+    # NTLK
+    data.path.append('./data/nltk_data')
+    logging.info(f'ntlk: {word_tokenize(text)}')
