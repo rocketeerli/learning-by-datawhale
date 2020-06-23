c02a864f0746aac34f975014a5699a8ab0a1e495
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2020-06-23 10:12:17 +0800

    get params

--- Issue10/task04/rcnn_basis.py
+++ Issue10/task04/rcnn_basis.py
@@ -20,6 +20,28 @@ def one_hot(x, n_class, dtype=torch.float32):
     return result
 
 
+def get_params():
+    ''' 初始化模型参数 '''
+    def _one(shape):
+        param = torch.zeros(shape, device=device, dtype=torch.float32)
+        nn.init.normal_(param, 0, 0.01)
+        return nn.Parameter(param)
+
+    # 隐藏层参数
+    W_xh = _one((num_inputs, num_hiddens))
+    W_hh = _one((num_hiddens, num_hiddens))
+    b_h = nn.Parameter(torch.zeros(num_hiddens, device=device))
+    # 输出层参数
+    W_hq = _one((num_hiddens, num_outputs))
+    b_q = nn.Parameter(torch.zeros(num_outputs, device=device))
+    return (W_xh, W_hh, b_h, W_hq, b_q)
+
+
+def rcnn(inputs, state, params):
+    ''' 定义模型 '''
+    
+
+
 def test():
     logging.info('测试 one hot 编码...')
     x = torch.tensor([0, 2])
@@ -32,5 +54,9 @@ def test():
 if __name__ == '__main__':
     # 加载数据集
     corpus_indices, char_to_idx, idx_to_char, vocab_size = load_data_jay_lyrics()
+    # 初始化参数
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    num_inputs, num_hiddens, num_outputs = vocab_size, 256, vocab_size
+
+    # 测试
     test()
