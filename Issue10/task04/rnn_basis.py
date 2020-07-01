fbf4551de2df465375c195791649d418793db744
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2020-07-01 23:57:16 +0800

    grad clip

--- Issue10/task04/rnn_basis.py
+++ Issue10/task04/rnn_basis.py
@@ -21,6 +21,11 @@ def one_hot(x, n_class, dtype=torch.float32):
 
 
 def to_onehot(X, n_class):
+    '''
+    :param X: (batch_size, timestep)
+    :param n_class: one hot 编码维度
+    :return: (timestep, batch_size, n_class)
+    '''
     return [one_hot(X[:, i], n_class) for i in range(X.shape[1])]
 
 
@@ -41,7 +46,7 @@ def get_params():
     return (W_xh, W_hh, b_h, W_hq, b_q)
 
 
-def rcnn(inputs, state, params):
+def rnn(inputs, state, params):
     ''' 定义模型 '''
     # inputs 和 outputs 皆为 num_steps 个形状为 (batch_size, vocab_size) 的矩阵
     W_xh, W_hh, b_h, W_hq, b_q = params
@@ -54,6 +59,22 @@ def rcnn(inputs, state, params):
     return outputs, (H,)
 
 
+def init_rnn_state(batch_size, num_hiddens, device):
+    ''' 初始化 rnn 的隐藏变量，即H0 '''
+    return (torch.zeros((batch_size, num_hiddens), device=device), )
+
+
+def grad_clipping(params, theta, device):
+    ''' 梯度裁剪 '''
+    norm = torch.tensor([0.0], device=device)
+    for param in params:
+        norm += (param.grad.data ** 2).sum()
+    norm = norm.sqrt().item()
+    if norm > theta:
+        for param in params:
+            param.grad.data *= (theta / norm)
+
+
 def test():
     logging.info('测试 one hot 编码...')
     x = torch.tensor([0, 2])
@@ -61,10 +82,23 @@ def test():
     logging.info(f'x one hot: {x_one_hot}')
     logging.info(f'shape of x one hot: {x_one_hot.shape}')
     logging.info(f'sum of x one hot: {x_one_hot.numpy().sum(axis=1)}')
+
+    logging.info('测试 one hot 编码一个 batch 的数据...')
     X = torch.arange(10).view(2, 5)
     inputs = to_onehot(X, vocab_size)
-    logging.info(f'length of inputs: {len(inputs)}')
-    logging.info(f'shape of inputs[0]: {inputs[0].shape}')
+    logging.info(f'时步 length of inputs: {len(inputs)}')
+    logging.info(f'每个时步的输入 shape of inputs[0]: {inputs[0].shape}')
+
+    logging.info('观察输出结果的个数，以及输出层和隐藏层的形状...')
+    logging.info(f'隐藏层维度: {num_hiddens} \t 输入和输出的维度: {vocab_size}')
+    state_init = init_rnn_state(X.shape[0], num_hiddens, device)
+    inputs = to_onehot(X.to(device), vocab_size)
+    params = get_params()
+    outputs, state_new = rnn(inputs, state_init, params)
+    logging.info(f'inputs length: {len(inputs)} \t shape: {inputs[0].shape}')
+    logging.info(f'outputs length: {len(outputs)} \t shape: {outputs[0].shape}')
+    logging.info(f'state init length: {len(state_init)} \t shape: {state_init[0].shape}')
+    logging.info(f'state new length: {len(state_new)} \t shape: {state_new[0].shape}')
 
 
 if __name__ == '__main__':
