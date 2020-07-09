16ba3ff9a81b24ae28eca7bb9589b3662fcdd700
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2020-07-10 00:21:39 +0800

    transformer model

--- Issue10/task06/transformer/transformer_model.py
+++ Issue10/task06/transformer/transformer_model.py
@@ -1,9 +1,10 @@
 # -*- coding:utf-8 -*-
+import math
 import numpy as np
 import torch
 from torch import nn
 import torch.nn.functional as F
-from utils import DotProductAttention, plot
+from utils import DotProductAttention, plot, Encoder, Decoder, EncoderDecoder, load_data_nmt, try_gpu, train_s2s_ch9, predict_s2s_ch9
 import logging
 
 logging.basicConfig(level=logging.INFO,
@@ -44,10 +45,11 @@ class MultiHeadAttention(nn.Module):
                 valid_length = torch.FloatTensor(np.tile(valid_length, self.num_heads))
             else:
                 valid_length = torch.FloatTensor(np.tile(valid_length, (self.num_heads, 1)))
-
             valid_length = valid_length.to(device)
 
+        # print('query: ', query, '\nshape: ', query.shape)
         output = self.attention(query, key, value, valid_length)
+        # print('output: ', output, '\nshape: ', output.shape)
         output_concat = transpose_output(output, self.num_heads)
         return self.W_o(output_concat)
 
@@ -118,6 +120,120 @@ class PositionalEncoding(nn.Module):
         return self.dropout(X)
 
 
+class EncoderBlock(nn.Module):
+    ''' 编码器
+    包含一个多头注意力层，一个position-wise FFN，和两个 Add and Norm 层
+    '''
+    def __init__(self, embedding_size, ffn_hidden_size, num_heads,
+                 dropout, **kwargs):
+        super().__init__(**kwargs)
+        self.attention = MultiHeadAttention(embedding_size, embedding_size, num_heads, dropout)
+        self.addnorm_1 = AddNorm(embedding_size, dropout)
+        self.ffn = PositionWiseFFN(embedding_size, ffn_hidden_size, embedding_size)
+        self.addnorm_2 = AddNorm(embedding_size, dropout)
+
+    def forward(self, X, valid_length):
+        Y = self.addnorm_1(X, self.attention(X, X, X, valid_length))
+        return self.addnorm_2(Y, self.ffn(Y))
+
+
+class TransformerEncoder(Encoder):
+    def __init__(self, vocab_size, embedding_size, ffn_hidden_size,
+                 num_heads, num_layers, dropout, **kwargs):
+        super().__init__(**kwargs)
+        self.embedding_size = embedding_size
+        self.embed = nn.Embedding(vocab_size, embedding_size)
+        self.pos_encoding = PositionalEncoding(embedding_size, dropout)
+        self.blks = nn.ModuleList()
+        for i in range(num_layers):
+            self.blks.append(
+                EncoderBlock(embedding_size, ffn_hidden_size,
+                             num_heads, dropout))
+
+    def forward(self, X, valid_length, *args):
+        # 把嵌入向量乘以 根号d 以防止其值过小。
+        X = self.pos_encoding(self.embed(X) * math.sqrt(self.embedding_size))
+        for blk in self.blks:
+            X = blk(X, valid_length)
+        return X
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, embedding_size, ffn_hidden_size, num_heads, dropout, i, **kwargs):
+        super().__init__(**kwargs)
+        self.i = i
+        self.attention_1 = MultiHeadAttention(embedding_size, embedding_size, num_heads, dropout)
+        self.addnorm_1 = AddNorm(embedding_size, dropout)
+        self.attention_2 = MultiHeadAttention(embedding_size, embedding_size, num_heads, dropout)
+        self.addnorm_2 = AddNorm(embedding_size, dropout)
+        self.ffn = PositionWiseFFN(embedding_size, ffn_hidden_size, embedding_size)
+        self.addnorm_3 = AddNorm(embedding_size, dropout)
+
+    def forward(self, X, state):
+        enc_outputs, enc_valid_length = state[0], state[1]
+
+        # state[2][self.i] stores all the previous t-1 query state of layer-i
+        # len(state[2]) = num_layers
+        # If training:
+        #     state[2] is useless.
+        # If predicting:
+        #     In the t-th timestep:
+        #         state[2][self.i].shape = (batch_size, t-1, hidden_size)
+        # Demo:
+        # love dogs ! [EOS]
+        #  |    |   |   |
+        #   Transformer 
+        #    Decoder
+        #  |   |   |   |
+        #  I love dogs !
+
+        if state[2][self.i] is None:
+            key_values = X
+        else:
+            # shape of key_values = (batch_size, t, hidden_size)
+            key_values = torch.cat((state[2][self.i], X), dim=1) 
+        state[2][self.i] = key_values
+
+        if self.training:
+            batch_size, seq_len, _ = X.shape
+            # Shape: (batch_size, seq_len), the values in the j-th column are j+1
+            valid_length = torch.FloatTensor(np.tile(np.arange(1, seq_len+1), (batch_size, 1))) 
+            valid_length = valid_length.to(X.device)
+        else:
+            valid_length = None
+
+        X2 = self.attention_1(X, key_values, key_values, valid_length)
+        Y = self.addnorm_1(X, X2)
+        Y2 = self.attention_2(Y, enc_outputs, enc_outputs, enc_valid_length)
+        Z = self.addnorm_2(Y, Y2)
+        return self.addnorm_3(Z, self.ffn(Z)), state
+
+
+class TransformerDecoder(Decoder):
+    def __init__(self, vocab_size, embedding_size, ffn_hidden_size,
+                 num_heads, num_layers, dropout, **kwargs):
+        super(TransformerDecoder, self).__init__(**kwargs)
+        self.embedding_size = embedding_size
+        self.num_layers = num_layers
+        self.embed = nn.Embedding(vocab_size, embedding_size)
+        self.pos_encoding = PositionalEncoding(embedding_size, dropout)
+        self.blks = nn.ModuleList()
+        for i in range(num_layers):
+            self.blks.append(
+                DecoderBlock(embedding_size, ffn_hidden_size, num_heads,
+                             dropout, i))
+        self.dense = nn.Linear(embedding_size, vocab_size)
+
+    def init_state(self, enc_outputs, enc_valid_length, *args):
+        return [enc_outputs, enc_valid_length, [None]*self.num_layers]
+
+    def forward(self, X, state):
+        X = self.pos_encoding(self.embed(X) * math.sqrt(self.embedding_size))
+        for blk in self.blks:
+            X, state = blk(X, state)
+        return self.dense(X), state
+
+
 def test():
     logging.info('多头注意力（Multi-head Attention Layers）...')
     cell = MultiHeadAttention(5, 9, 3, 0.5)
@@ -145,9 +261,41 @@ def test():
     logging.info('位置编码 ...')
     pe = PositionalEncoding(20, 0)
     Y = pe(torch.zeros((1, 100, 20))).numpy()
-    plot(np.arange(100), Y[0, :, 4:8].T, figsize=(6, 2.5),
-             legend=["dim %d" % p for p in [4, 5, 6, 7]])
+    # plot(np.arange(100), Y[0, :, 4:8].T, figsize=(6, 2.5),
+    #          legend=["dim %d" % p for p in [4, 5, 6, 7]])
+
+    logging.info('编码器模块 EncoderBlock ...')
+    X = torch.ones((2, 100, 24))
+    encoder_blk = EncoderBlock(24, 48, 8, 0.5)
+    logging.info(f'endcoer output shape: {encoder_blk(X, valid_length).shape}')
+
+    logging.info('Transformer 编码器 ...')
+    encoder = TransformerEncoder(200, 24, 48, 8, 2, 0.5)
+    logging.info(f'encoder output shape: {encoder(torch.ones((2, 100)).long(), valid_length).shape}')
 
 
 if __name__ == '__main__':
-    test()
+    # test()
+
+    embed_size, embedding_size, num_layers, dropout = 32, 32, 2, 0.05
+    batch_size, num_steps = 64, 10
+    lr, num_epochs, ctx = 0.005, 250, try_gpu()
+    logging.info(f'using {ctx} ...')
+    num_hiddens, num_heads = 64, 4
+
+    src_vocab, tgt_vocab, train_iter = load_data_nmt(batch_size, num_steps)
+
+    encoder = TransformerEncoder(
+        len(src_vocab), embedding_size, num_hiddens, num_heads, num_layers,
+        dropout)
+    decoder = TransformerDecoder(
+        len(src_vocab), embedding_size, num_hiddens, num_heads, num_layers,
+        dropout)
+    model = EncoderDecoder(encoder, decoder)
+    train_s2s_ch9(model, train_iter, lr, num_epochs, ctx)
+
+    # 测试模型
+    model.eval()
+    for sentence in ['Go .', 'Wow !', "I'm OK .", 'I won !']:
+        print(sentence + ' => ' + predict_s2s_ch9(
+            model, sentence, src_vocab, tgt_vocab, num_steps, ctx))
--- Issue10/task06/transformer/utils.py
+++ Issue10/task06/transformer/utils.py
@@ -1,8 +1,11 @@
 # -*- coding:utf-8 -*-
 import math
+from base import Vocab
+from torch.utils import data
 import torch
-from torch import nn
+from torch import nn, optim
 import matplotlib.pyplot as plt
+import time
 
 
 def SequenceMask(X, X_len, value=-1e6):
@@ -16,7 +19,7 @@ def SequenceMask(X, X_len, value=-1e6):
     maxlen = X.size(1)
     X_len = X_len.to(X.device)
     mask = torch.arange((maxlen), dtype=torch.float, device=X.device)
-    mask = mask[None, :] < X_len[:, None]
+    mask = mask[None, :] < X_len.float()[:, None]
     X[~mask] = value
     return X
 
@@ -30,7 +33,9 @@ def masked_softmax(X, valid_length):
         shape = X.shape
         if valid_length.dim() == 1:
             try:
+                # print('valid length: ', valid_length, 'shape: ', valid_length.shape)
                 valid_length = torch.FloatTensor(valid_length.numpy().repeat(shape[1], axis=0))
+                # print('valid length: ', valid_length, 'shape: ', valid_length.shape)
             except:
                 valid_length = torch.FloatTensor(valid_length.cpu().numpy().repeat(shape[1], axis=0))
         else:
@@ -99,3 +104,177 @@ def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
     axes.set_ylim(ylim)
     if legend: axes.legend(legend)
     axes.grid()
+
+
+class Encoder(nn.Module):
+    def __init__(self, **kwargs):
+        super(Encoder, self).__init__(**kwargs)
+
+    def forward(self, X, *args):
+        raise NotImplementedError
+
+
+class Decoder(nn.Module):
+    def __init__(self, **kwargs):
+        super(Decoder, self).__init__(**kwargs)
+
+    def init_state(self, enc_outputs, *args):
+        raise NotImplementedError
+
+    def forward(self, X, state):
+        raise NotImplementedError
+
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(EncoderDecoder, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, enc_X, dec_X, *args):
+        enc_outputs = self.encoder(enc_X, *args)
+        dec_state = self.decoder.init_state(enc_outputs, *args)
+        return self.decoder(dec_X, dec_state)
+
+
+def load_data_nmt(batch_size, max_len, num_examples=1000):
+    """Download an NMT dataset, return its vocabulary and data iterator."""
+    # Download and preprocess
+    def preprocess_raw(text):
+        text = text.replace('\u202f', ' ').replace('\xa0', ' ')
+        out = ''
+        for i, char in enumerate(text.lower()):
+            if char in (',', '!', '.') and text[i-1] != ' ':
+                out += ' '
+            out += char
+        return out 
+
+    with open('./data/fraeng3479/fra.txt', 'r', encoding='utf-8') as f:
+        raw_text = f.read()
+
+    text = preprocess_raw(raw_text)
+
+    # Tokenize
+    source, target = [], []
+    for i, line in enumerate(text.split('\n')):
+        if i >= num_examples:
+            break
+        parts = line.split('\t')
+        if len(parts) >= 2:
+            source.append(parts[0].split(' '))
+            target.append(parts[1].split(' '))
+
+    # Build vocab
+    def build_vocab(tokens):
+        tokens = [token for line in tokens for token in line]
+        return Vocab(tokens, min_freq=3, use_special_tokens=True)
+    src_vocab, tgt_vocab = build_vocab(source), build_vocab(target)
+
+    # Convert to index arrays
+    def pad(line, max_len, padding_token):
+        if len(line) > max_len:
+            return line[:max_len]
+        return line + [padding_token] * (max_len - len(line))
+
+    def build_array(lines, vocab, max_len, is_source):
+        lines = [vocab[line] for line in lines]
+        if not is_source:
+            lines = [[vocab.bos] + line + [vocab.eos] for line in lines]
+        array = torch.tensor([pad(line, max_len, vocab.pad) for line in lines])
+        valid_len = (array != vocab.pad).sum(1)
+        return array, valid_len
+
+    src_vocab, tgt_vocab = build_vocab(source), build_vocab(target)
+    src_array, src_valid_len = build_array(source, src_vocab, max_len, True)
+    tgt_array, tgt_valid_len = build_array(target, tgt_vocab, max_len, False)
+    train_data = data.TensorDataset(src_array, src_valid_len, tgt_array, tgt_valid_len)
+    train_iter = data.DataLoader(train_data, batch_size, shuffle=True)
+    return src_vocab, tgt_vocab, train_iter
+
+
+def try_gpu():
+    """If GPU is available, return torch.device as cuda:0; else return torch.device as cpu."""
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu')
+    return device
+
+
+def grad_clipping(params, theta, device):
+    """Clip the gradient."""
+    norm = torch.tensor([0], dtype=torch.float32, device=device)
+    for param in params:
+        norm += (param.grad ** 2).sum()
+    norm = norm.sqrt().item()
+    if norm > theta:
+        for param in params:
+            param.grad.data.mul_(theta / norm)
+
+
+def grad_clipping_nn(model, theta, device):
+    """Clip the gradient for a nn model."""
+    grad_clipping(model.parameters(), theta, device)
+
+
+class MaskedSoftmaxCELoss(nn.CrossEntropyLoss):
+    def forward(self, pred, label, valid_length):
+        # the sample weights shape should be (batch_size, seq_len)
+        # logging.info(f'pred : {pred.size()} \t label: {label.size()} \t valid_length: {valid_length.size()}')
+        # print('pred: ', pred, '\n', 'label: ', label, 'valid_length: ', valid_length)
+        weights = torch.ones_like(label)
+        weights = SequenceMask(weights, valid_length, 0).float()
+        self.reduction = 'none'
+        output = super().forward(pred.transpose(1, 2), label)
+        return (output*weights).mean(dim=1)
+
+
+def train_s2s_ch9(model, data_iter, lr, num_epochs, device):  # Saved in d2l
+    model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    loss = MaskedSoftmaxCELoss()
+    tic = time.time()
+    for epoch in range(1, num_epochs+1):
+        l_sum, num_tokens_sum = 0.0, 0.0
+        for batch in data_iter:
+            optimizer.zero_grad()
+            X, X_vlen, Y, Y_vlen = [x.to(device) for x in batch]
+            Y_input, Y_label, Y_vlen = Y[:, :-1], Y[:, 1:], Y_vlen-1
+
+            Y_hat, _ = model(X, Y_input, X_vlen, Y_vlen)
+            l = loss(Y_hat, Y_label, Y_vlen).sum()
+            l.backward()
+
+            with torch.no_grad():
+                grad_clipping_nn(model, 5, device)
+            num_tokens = Y_vlen.sum().item()
+            optimizer.step()
+            l_sum += l.sum().item()
+            num_tokens_sum += num_tokens
+        if epoch % 50 == 0:
+            print("epoch {0:4d}, loss {1:.3f}, time {2:.1f} sec".format( 
+                  epoch, (l_sum/num_tokens_sum), time.time()-tic))
+            tic = time.time()
+
+
+def predict_s2s_ch9(model, src_sentence, src_vocab, tgt_vocab, max_len, device):
+    src_tokens = src_vocab[src_sentence.lower().split(' ')]
+    src_len = len(src_tokens)
+    if src_len < max_len:
+        src_tokens += [src_vocab.pad] * (max_len - src_len)
+    enc_X = torch.tensor(src_tokens, device=device)
+    enc_valid_length = torch.tensor([src_len], device=device)
+    # use expand_dim to add the batch_size dimension.
+    enc_outputs = model.encoder(enc_X.unsqueeze(dim=0), enc_valid_length)
+    dec_state = model.decoder.init_state(enc_outputs, enc_valid_length)
+    dec_X = torch.tensor([tgt_vocab.bos], device=device).unsqueeze(dim=0)
+    predict_tokens = []
+    for _ in range(max_len):
+        Y, dec_state = model.decoder(dec_X, dec_state)
+        # The token with highest score is used as the next time step input.
+        dec_X = Y.argmax(dim=2)
+        py = dec_X.squeeze(dim=0).int().item()
+        if py == tgt_vocab.eos:
+            break
+        predict_tokens.append(py)
+    return ' '.join(tgt_vocab.to_tokens(predict_tokens))
