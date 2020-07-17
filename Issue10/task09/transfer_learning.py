42a78566257e9721ecd5cd73f503aec9db3fba98
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2020-07-17 22:49:22 +0800

    finish transfer learning

--- Issue10/task09/transfer_learning.py
+++ Issue10/task09/transfer_learning.py
@@ -56,6 +56,83 @@ def get_styles(image_shape, device):
     return style_X, styles_Y
 
 
+def content_loss(Y_hat, Y):
+    ''' 内容损失 '''
+    return F.mse_loss(Y_hat, Y)
+
+
+def gram(X):
+    ''' 计算特征图的格莱姆矩阵 '''
+    num_channels, n = X.shape[1], X.shape[2] * X.shape[3]
+    X = X.view(num_channels, n)
+    return torch.matmul(X, X.t()) / (num_channels * n)
+
+
+def style_loss(Y_hat, gram_Y):
+    ''' 样式损失 '''
+    return F.mse_loss(gram(Y_hat), gram_Y)
+
+
+def tv_loss(Y_hat):
+    ''' 总变差损失 '''
+    return 0.5 * (F.l1_loss(Y_hat[:, :, 1:, :], Y_hat[:, :, :-1, :]) +
+                  F.l1_loss(Y_hat[:, :, :, 1:], Y_hat[:, :, :, :-1]))
+
+
+def compute_loss(X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram):
+    ''' 分别计算内容损失、样式损失和总变差损失 '''
+    contents_l = [content_loss(Y_hat, Y) * content_weight for Y_hat, Y in zip(
+        contents_Y_hat, contents_Y)]
+    styles_l = [style_loss(Y_hat, Y) * style_weight for Y_hat, Y in zip(
+        styles_Y_hat, styles_Y_gram)]
+    tv_l = tv_loss(X) * tv_weight
+    # 对所有损失求和
+    l = sum(styles_l) + sum(contents_l) + tv_l
+    return contents_l, styles_l, tv_l, l
+
+
+class GeneratedImage(torch.nn.Module):
+    def __init__(self, img_shape):
+        super().__init__()
+        self.weight = torch.nn.Parameter(torch.rand(*img_shape))
+
+    def forward(self):
+        return self.weight
+
+
+def get_inits(X, device, lr, styles_Y):
+    gen_img = GeneratedImage(X.shape).to(device)
+    gen_img.weight.data = X.data
+    optimizer = torch.optim.Adam(gen_img.parameters(), lr=lr)
+    styles_Y_gram = [gram(Y) for Y in styles_Y]
+    return gen_img(), styles_Y_gram, optimizer
+
+
+def train(X, contents_Y, styles_Y, device, lr, max_epochs, lr_decay_epoch):
+    print("training on ", device)
+    X, styles_Y_gram, optimizer = get_inits(X, device, lr, styles_Y)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, lr_decay_epoch, gamma=0.1)
+    for i in range(max_epochs):
+        start = time.time()
+
+        contents_Y_hat, styles_Y_hat = extract_features(
+                X, content_layers, style_layers)
+        contents_l, styles_l, tv_l, l = compute_loss(
+                X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram)
+
+        optimizer.zero_grad()
+        l.backward(retain_graph=True)
+        optimizer.step()
+        scheduler.step()
+
+        if i % 50 == 0 and i != 0:
+            print('epoch %3d, content loss %.2f, style loss %.2f, '
+                  'TV loss %.2f, %.2f sec'
+                  % (i, sum(contents_l).item(), sum(styles_l).item(), tv_l.item(),
+                     time.time() - start))
+    return X.detach()
+
+
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logging.info(f'use device: {device}, \t version of torch: {torch.__version__}')
@@ -78,3 +155,26 @@ if __name__ == '__main__':
     for i in range(max(content_layers + style_layers) + 1):
         net_list.append(pretrained_net.features[i])
     net = torch.nn.Sequential(*net_list)
+
+    # 损失函数权值
+    content_weight, style_weight, tv_weight = 1, 1e3, 10
+
+    # 开始训练
+    image_shape = (150, 225)
+    net = net.to(device)
+    content_X, contents_Y = get_contents(image_shape, device)
+    style_X, styles_Y = get_styles(image_shape, device)
+    output = train(content_X, contents_Y, styles_Y, device, 0.01, 500, 200)
+
+    plt.imshow(postprocess(output))
+    plt.show()
+
+    # 在更大的尺寸上训练
+    image_shape = (300, 450)
+    _, content_Y = get_contents(image_shape, device)
+    _, style_Y = get_styles(image_shape, device)
+    X = preprocess(postprocess(output), image_shape).to(device)
+    big_output = train(X, content_Y, style_Y, device, 0.01, 500, 200)
+
+    plt.imshow(postprocess(big_output))
+    plt.show()
